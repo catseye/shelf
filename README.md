e10cae6310b2a505532cac10949ab5a5e7a50810
Chris Pressey <cpressey@catseye.tc>
2016-12-28 17:51:51 +0000

    Add shelf's source code as it exists today. Minimally write README.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# shelf
-[WIP] A package installer which neither packages nor installs
+shelf
+=====
+
+**shelf** is "a package installer which neither packages nor installs".
+
+Work in progress.
+
+shelf aims to replace [toolshelf](https://github.com/catseye/toolshelf).
+
+shelf is written in Bourne shell.
+
+Quick Start
+-----------
+
+Put the file `shelf.sh` somewhere, say `$HOME/shelf.sh`.
+
+Add these two lines to the end of your shell startup script (`.bashrc`,
+`.bash_profile`, or whatever):
+
+    . $HOME/shelf.sh
+    shelf_init $HOME/.local
+
+Then start a new shell for it to take effect.
+
+Usage
+-----
+
+The following shell functions are defined by `shelf.sh` and available for use:
+
+*   `shelf_link` *DIR*
+
+*   `shelf_unlink` *DIR*
+
+*   `shelf_unlink_broken`
+
+*   `shelf_build` *DIR*
---
+++ shelf.sh
@@ -0,0 +1,151 @@
+# encoding: UTF-8
+
+# This file is intended to be `source`d (or `.`ed) from `.bashrc` or similar.
+
+
+### utility functions ###
+
+
+function _shelf_verbose() {
+    if [ -e /tmp/.shelf_verbose ]; then
+        echo $*
+    fi
+}
+
+function _shelf_show_run() {
+    echo $*
+    $*
+}
+
+function _shelf_ln() {
+    source="$1"
+    dest="$2"
+    if [ -e "$dest" ]; then
+        _shelf_verbose $dest already exists
+    else
+        _shelf_show_run ln -s `realpath "$source"` "$dest"
+    fi
+}
+
+function _shelf_link_stuff() {
+    dir="$1"
+    subdir="$2"
+    find_opts="-name .git -prune -o -path Funicular/eg -prune -o -path Chrysoberyl/modules -prune -o -path Chrysoberyl/checkout -prune -o $3"
+    skip_pat="$4"
+    if [ "X$dir" = X ]; then
+        echo "Usage: shelf_link_stuff <dir> <subdir> <find-opts> <skip-pat>"
+        return 1
+    fi
+    _shelf_verbose "find $dir $find_opts"
+    for source in `find "$dir" $find_opts`; do
+        base=`basename "$source"`
+        case "$base" in
+            ${skip_pat})
+                _shelf_verbose Skipping $base
+            ;;
+            *)
+                _shelf_ln "$source" "$SHELF_FARM/$subdir/$base"
+            ;;
+        esac
+    done
+}
+
+function _shelf_link_bin() {
+    subdir="bin"
+    find_opts="-executable -type f"
+    skip_pat=".git|depcomp|configure|config.guess|*.h|*.so|*.so.*"
+    _shelf_link_stuff "$1" "$subdir" "$find_opts" "$skip_pat"
+}
+
+function _shelf_link_include() {
+    subdir="include"
+    find_opts="-name '*.h'"
+    skip_pat=".git"
+    _shelf_link_stuff "$1" "$subdir" "$find_opts" "$skip_pat"
+}
+
+function _shelf_link_lib() {
+    subdir="lib"
+    find_opts="-name '*.so' -o -name '*.so.*'"
+    skip_pat=".git"
+    _shelf_link_stuff "$1" "$subdir" "$find_opts" "$skip_pat"
+}
+
+
+### public functions ###
+
+
+function shelf_init() {
+    export SHELF_FARM="$1"
+    export PATH="$SHELF_FARM/bin:$PATH"
+    export LD_LIBRARY_PATH="$SHELF_FARM/lib:$LD_LIBRARY_PATH"
+    export LIBRARY_PATH="$SHELF_FARM/lib:$LIBRARY_PATH"
+    export C_INCLUDE_PATH="$SHELF_FARM/include:$C_INCLUDE_PATH"
+    export CPLUS_INCLUDE_PATH="$SHELF_FARM/include:$CPLUS_INCLUDE_PATH"
+    export PYTHONPATH="$SHELF_FARM/python:$PYTHONPATH"
+    export PKG_CONFIG_PATH="$SHELF_FARM/pkgconfig:$PKG_CONFIG_PATH"
+    export LUA_PATH="$SHELF_FARM/lua/?.lua;$LUA_PATH"
+    export LUA_CPATH="$SHELF_FARM/lib/?.so;$LUA_CPATH"
+}
+
+function shelf_link() {
+    if [ "X$1" = X ]; then
+        echo "Usage: shelf_link {dir}"
+        return 1
+    fi
+    for dir in $*; do
+        if [ -e "$dir/install" ]; then
+            dir="$dir/install"
+        fi
+        _shelf_link_bin "$dir"
+        _shelf_link_include "$dir"
+        _shelf_link_lib "$dir"
+    done
+}
+
+function shelf_unlink() {
+    if [ "X$1" = X ]; then
+        echo "Usage: shelf_unlink {dir}"
+        return 1
+    fi
+    for dir in $*; do
+        dir=`realpath "$dir"`
+        for sub in bin include lib; do
+            for file in $SHELF_FARM/$sub/*; do
+                link=`readlink -f "$file"`
+                case $link in
+                    ${dir}*)
+                        _shelf_show_run rm "$file"
+                    ;;
+                    *)
+                    ;;
+                esac
+            done
+        done
+    done
+}
+
+function shelf_unlink_broken() {
+    for sub in bin include lib; do
+        for file in $SHELF_FARM/$sub/*; do
+            if [ ! -e "$file" ]; then
+                _shelf_show_run rm "$file"
+            fi
+        done
+    done
+}
+
+function shelf_build() {
+    dir="$1"
+    dir=`realpath "$dir"`
+    if [ -x "$dir/build.sh" ]; then
+        CWD=`pwd`
+        cd $dir
+        ./build.sh
+        cd $CWD
+        return $?
+    else
+        echo "No heuristic to build this source"
+        return 1
+    fi
+}
