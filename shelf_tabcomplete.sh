3b93d6c3f8e6d53229c5b1d4fbd6ab4898abede5
Chris Pressey <cpressey@catseye.tc>
2017-09-06 16:23:24 +0100

    Add a rudimentary bash tab-completion function for shelf_cd.

---
+++ shelf_tabcomplete.sh
@@ -0,0 +1,23 @@
+# tab-completion setup for shelf in bash
+# source this file after shelf has been loaded.
+# note, I do not claim to entirely know what I'm doing here.
+
+function _shelf_cd_tabcomplete_()
+{
+    local cmd="${1##*/}"
+    local word=${COMP_WORDS[COMP_CWORD]}
+    local line=${COMP_LINE}
+
+    local path=`echo "$SHELF_PATH" | sed -e 's/:/ /g'`
+    local subdirs=''
+    for d in $path; do
+        if [ -d $d ]; then
+            these=`ls $d`
+            subdirs="$subdirs $these"
+        fi
+    done
+
+    COMPREPLY=($(compgen -W "${subdirs}" "${word}"))
+}
+
+complete -F _shelf_cd_tabcomplete_ shelf_cd
