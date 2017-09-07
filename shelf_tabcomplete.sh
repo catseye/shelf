b0e05ad968dc0dc10bc7b8af07ac2fa7dfbc190b
Chris Pressey <cpressey@catseye.tc>
2017-09-07 16:14:06 +0100

    Autocomplete on shelf_which as well.

--- shelf_tabcomplete.sh
+++ shelf_tabcomplete.sh
@@ -21,3 +21,18 @@ function _shelf_cd_tabcomplete_()
 }
 
 complete -F _shelf_cd_tabcomplete_ shelf_cd
+
+
+function _shelf_which_tabcomplete_()
+{
+    local cmd="${1##*/}"
+    local word=${COMP_WORDS[COMP_CWORD]}
+    local line=${COMP_LINE}
+
+    local path=`echo "$SHELF_PATH" | sed -e 's/:/ /g'`
+    local files=`ls $SHELF_FARMBASE/bin`
+
+    COMPREPLY=($(compgen -W "${files}" "${word}"))
+}
+
+complete -F _shelf_which_tabcomplete_ shelf_which
